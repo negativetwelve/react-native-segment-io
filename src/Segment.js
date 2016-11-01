213da1a22c4597a6994745ba7d00bc5c5dc98471
Mark Miyashita <negativetwelve@gmail.com>
2016-11-01 07:38:42 -0700

    Add JS methods that we expect from the native modules

--- src/Segment.js
+++ src/Segment.js
@@ -7,6 +7,39 @@ const {RNSegment} = NativeModules;
 
 class Segment {
 
+  // --------------------------------------------------
+  // Initialize
+  // --------------------------------------------------
+  initialize(options) {
+    return RNSegment.initialize(options);
+  }
+
+  reset() {
+    return RNSegment.reset();
+  }
+
+  // --------------------------------------------------
+  // Identity
+  // --------------------------------------------------
+  identify(userId, traits, options) {
+    return RNSegment.identify(userId, traits, options);
+  }
+
+  alias(userId, options) {
+    return RNSegment.alias(userId, options);
+  }
+
+  // --------------------------------------------------
+  // Track
+  // --------------------------------------------------
+  track(properties, options) {
+    return RNSegment.track(properties, options);
+  }
+
+  screen(name, properties, options) {
+    return RNSegment.screen(name, properties, options);
+  }
+
 }
 
 
