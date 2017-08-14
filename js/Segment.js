3c1bef91d13360f8baaf29a45588e25a1fd3b194
Mark Miyashita <negativetwelve@gmail.com>
2017-08-14 12:32:06 -0700

    Change JS implementation to use Package.create

--- js/Segment.js
+++ js/Segment.js
@@ -1,52 +1,38 @@
 // Libraries
-import {NativeModules} from 'react-native';
-
-// Native Modules
-const {RNSegment} = NativeModules;
+import {NativeModules, Platform} from 'react-native';
+import Package from 'react-native-package';
 
 
-class Segment {
+/**
+ * Package.create handles two things:
+ *
+ *   1. Checks that for each platform that's `enabled`, the module is installed
+ *      properly. If it's not, it logs a warning.
+ *   2. Guards the module on every platform that is not `enabled`. This allows
+ *      the module to exist in cross-platform code without hacks to disable it.
+ *
+ * You can read more about `react-native-package` here:
+ * https://github.com/negativetwelve/react-native-package
+ */
+export default Package.create({
+  json: require('../package.json'),
+  nativeModule: NativeModules.RNSegment,
+  enabled: Platform.select({
+    ios: true,
+  }),
+  export: (Segment) => ({
+    // Initialize
+    initialize: (options) => Segment.initialize(options),
+    reset: () => Segment.reset(),
 
-  // --------------------------------------------------
-  // Initialize
-  // --------------------------------------------------
-  initialize(options) {
-    return RNSegment.initialize(options);
-  }
-
-  reset() {
-    return RNSegment.reset();
-  }
-
-  // --------------------------------------------------
-  // Identity
-  // --------------------------------------------------
-  identify(userId, traits) {
+    // Identity
     // Native side requires a string for the userId.
-    return RNSegment.identify(userId.toString(), traits);
-  }
+    identify: (userId, traits) => Segment.identify(userId.toString(), traits),
+    alias: (userId) => Segment.alias(userId.toString()),
 
-  alias(userId) {
-    // Native side requires a string for the userId.
-    return RNSegment.alias(userId.toString());
-  }
-
-  // --------------------------------------------------
-  // Track
-  // --------------------------------------------------
-  track(name, properties) {
-    return RNSegment.track(name, properties);
-  }
-
-  screen(name, properties) {
-    return RNSegment.screen(name, properties);
-  }
-
-  flush() {
-    return RNSegment.flush();
-  }
-
-}
-
-
-export default new Segment();
+    // Track
+    track: (name, properties) => Segment.track(name, properties),
+    screen: (name, properties) => Segment.screen(name, properties),
+    flush: () => Segment.flush(),
+  }),
+});
